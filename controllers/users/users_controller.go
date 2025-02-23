374ef6a97e5d196842a76a353498e65ed04721b5
l.cobasgomero@betacom.it <l.cobasgomero@betacom.it>
2025-02-23 23:20:29 +0100

    add update user functionality with support for partial updates

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -44,6 +44,36 @@ func GetUser(c *gin.Context) {
 	c.JSON(http.StatusOK, user)
 }
 
+func UpdateUser(c *gin.Context) {
+
+	userId, err := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if err != nil {
+		restErr := errors.NewBadRequestError("user id should be a number")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	var user users.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	user.Id = userId
+
+	isPartial := c.Request.Method == http.MethodPatch
+
+	result, UpdateErr := services.UpdateUser(isPartial, user)
+	if UpdateErr != nil {
+		c.JSON(UpdateErr.Status, UpdateErr)
+		return
+	}
+
+	c.JSON(http.StatusOK, result)
+
+}
+
 func FindUser(c *gin.Context) {
 	c.String(http.StatusNotImplemented, "implement me!")
 }
